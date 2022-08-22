cf4712cd3561815c475b50736dbd78e82f5549eb
Adrian Kovatana <adrian.kovatana@gmail.com>
2022-08-22 16:27:38 -0500

    Add scriptable object with a game over event

---
+++ Assets/_Game/Scripts/Test/GameOverTest.cs
@@ -0,0 +1,36 @@
+using DragonLens.BrackeysGameJam2022_2.GameStates;
+using UnityEngine;
+
+namespace DragonLens.BrackeysGameJam2022_2.Tests
+{
+    public class GameOverTest : MonoBehaviour
+    {
+        [SerializeField] private GameOverStateData _gameOverData;
+        [SerializeField] private GameObject[] _disabledOnStart;
+        [SerializeField] private GameObject[] _toEnable;
+        [SerializeField] private GameObject[] _toDisable;
+
+        private void Start() {
+            foreach(GameObject go in _disabledOnStart) {
+                go.SetActive(false);
+            }
+        }
+
+        private void OnEnable() {
+            _gameOverData.OnGameOver += OnGameOver;
+        }
+
+        private void OnDisable() {
+            _gameOverData.OnGameOver -= OnGameOver;
+        }
+
+        private void OnGameOver() {
+            foreach(GameObject go in _toDisable) {
+                go.SetActive(false);
+            }
+            foreach(GameObject go in _toEnable) {
+                go.SetActive(true);
+            }
+        }
+    }
+}
