df96ce61d291997d4be17c99d3c30e33517107e3
Adrian Kovatana <adrian.kovatana@gmail.com>
2022-08-21 19:08:59 -0500

    Add state machine base classes

---
+++ Assets/_Game/Scripts/GameState/StateMachine.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace AdrianKovatana.Essentials.FiniteStateMachine
+{
+	public abstract class StateMachine : MonoBehaviour
+	{
+		public IState CurrentState { get; private set; }
+
+		private bool _inTransition = false;
+
+		public void ChangeState(IState newState) {
+			if(CurrentState == newState || _inTransition) return;
+
+            _inTransition = true;
+
+            if(CurrentState != null) CurrentState.StateExit();
+
+            CurrentState = newState;
+
+            if(CurrentState != null) CurrentState.StateEnter();
+
+            _inTransition = false;
+        }
+
+		private void Update() {
+			if(CurrentState == null) return;
+			if(_inTransition) return;
+
+			CurrentState.StateUpdate();
+		}
+
+		private void FixedUpdate() {
+            if(CurrentState == null) return;
+            if(_inTransition) return;
+
+			CurrentState.StateFixedUpdate();
+		}
+	}
+}
