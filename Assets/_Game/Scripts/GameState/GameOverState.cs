8f3fa4e51aebbde337d485ad28618f7e6880478c
Adrian Kovatana <adrian.kovatana@gmail.com>
2022-08-21 19:10:29 -0500

    Add puzzle state machine and its states

---
+++ Assets/_Game/Scripts/GameState/GameOverState.cs
@@ -0,0 +1,16 @@
+﻿using AdrianKovatana.Essentials.FiniteStateMachine;
+using UnityEngine;
+
+namespace DragonLens.BrackeysGameJam2022_2.GameStates
+{
+    public class GameOverState : MonoBehaviour, IState
+    {
+        public void StateEnter() {
+            // Raise game over event to notify listeners
+            print("Game over");
+        }
+        public void StateUpdate() { }
+        public void StateFixedUpdate() { }
+        public void StateExit() { }
+    }
+}
---
+++ Assets/_Game/Scripts/GameState/PuzzleStateMachine.cs
@@ -0,0 +1,14 @@
+﻿using AdrianKovatana.Essentials.FiniteStateMachine;
+using UnityEngine;
+
+namespace DragonLens.BrackeysGameJam2022_2.GameStates
+{
+    public class PuzzleStateMachine : StateMachine
+    {
+        [SerializeField] private InitializationState _initializationState;
+
+        private void Start() {
+            ChangeState(_initializationState);
+        }
+    }
+}
