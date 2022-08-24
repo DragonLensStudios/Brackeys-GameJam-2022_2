da2fc63bab820ad34801fd7183dc450a48fc457a
Billy <se.billy.mccarthy@gmail.com>
2022-08-24 12:13:50 +0100

    Fixup lighting scripts to have smoother flow, clean up code to remove loops

--- Assets/Lighting/Scripts/LightFlicker.cs
+++ Assets/Lighting/Scripts/LightFlicker.cs
@@ -1,96 +1,62 @@
-using System.Collections.Generic;
 using UnityEngine.Rendering.Universal;
 using UnityEngine;
 
 public class LightFlicker : MonoBehaviour
 {
     // Start is called before the first frame update
-    private List<Light2D> lights = new List<Light2D>();
-    private int nextUpdate = 1;
-    private int currentFrameCount = 0;
-    private bool innerLightOn = true;
-    private bool outerLightOn = true;
-    private bool isInnersTurn = false;
-    private bool isOutersTurn = true;
+    private Light2D flickerLight;
+    private bool isExpanding = false;
+    private int frameCount = 0;
 
-    public float flickerLowerFrequency;
-    public float flickerUpperFrequency;
+    public float flickerLightMaxRadius;
+    public float flickerLightMinRadius;
+    public float radiusIncrementPerUpdate;
+    public float intensityIncrementPerUpdate;
+    public int framesPerLightUpdate;
 
     void Start()
     {
-        GameObject[] flickers = GameObject.FindGameObjectsWithTag("Flicker");
-        foreach(GameObject gameObject in flickers)
-        {
-            lights.Add(GameObjectToLight2D(gameObject));
-        }
+        flickerLight = GameObject.Find("Candle Light 2D Flicker").GetComponent<Light2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        frameCount++;
 
-        currentFrameCount++;
-        if (currentFrameCount >= nextUpdate)
+        if(frameCount >= framesPerLightUpdate)
         {
-            //Randomise when the next update will come, this can be done via the object this script is attached to. 
-            nextUpdate =((int)Random.Range(flickerLowerFrequency, flickerUpperFrequency));
-            UpdateLights();
-            currentFrameCount = 0;
+            FlickerLight();
+            frameCount = 0;
         }
     }
 
-    void UpdateLights()
+    void FlickerLight()
     {
-        float target = Mathf.InverseLerp(flickerLowerFrequency, flickerUpperFrequency, nextUpdate);
-        float intensity = Mathf.SmoothStep(0.25f, 0.5f, target);
-        for (int i = 0; i < lights.Count; i++)
+        if(isExpanding)
         {
-            Light2D light = lights[i];
-            
-            //Turn inside light off
-            if (innerLightOn && !outerLightOn && isInnersTurn && light.name == "Candle Light 2D Flicker Inner")
+            if (flickerLight.pointLightOuterRadius < flickerLightMaxRadius)
             {
-                light.intensity = intensity;
-                innerLightOn = false;
-                isOutersTurn = false;
-                isInnersTurn = true;
-                break;
+                flickerLight.pointLightOuterRadius += radiusIncrementPerUpdate;
+                flickerLight.intensity += intensityIncrementPerUpdate;
             }
-
-            //Turn inside light on 
-            if (!innerLightOn && !outerLightOn && light.name == "Candle Light 2D Flicker Inner")
+            else 
             {
-                light.intensity = intensity;
-                innerLightOn = true;
-                isInnersTurn = false;
-                isOutersTurn = true;
-                break;
+                isExpanding = false;
             }
-
-            //Turn outside light off
-            if (outerLightOn && innerLightOn && light.name == "Candle Light 2D Flicker Outer")
+        } 
+        else
+        {
+            if (flickerLight.pointLightOuterRadius > flickerLightMinRadius)
             {
-                light.intensity = intensity;
-                outerLightOn = false;
-                isInnersTurn = true;
-                isOutersTurn = false;
-                break;
+                flickerLight.pointLightOuterRadius -= radiusIncrementPerUpdate;
+                flickerLight.intensity -= intensityIncrementPerUpdate;
             }
-
-            //Turn outside light on 
-            if (innerLightOn && !outerLightOn && isOutersTurn && light.name == "Candle Light 2D Flicker Outer")
+            else
             {
-                light.intensity = intensity;
-                outerLightOn = true;
-                isOutersTurn = true;
-                isInnersTurn = false;
-                break;
+                isExpanding = true;
             }
         }
     }
 
-    public static Light2D GameObjectToLight2D(GameObject gameObject)
-    {
-        return gameObject.GetComponent<Light2D>();
-    }
 }
