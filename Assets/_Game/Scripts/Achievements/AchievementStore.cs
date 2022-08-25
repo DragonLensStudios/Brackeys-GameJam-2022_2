6b66087963132fcc75a971fe74c26c215790fb33
Billy <se.billy.mccarthy@gmail.com>
2022-08-25 22:24:53 +0100

    Load achievements from flat file and display in Pause menu

---
+++ Assets/_Game/Scripts/Achievements/AchievementStore.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class AchievementStore : MonoBehaviour
+{
+    public Achievements achievements;
+    public string path = "Assets/_Game/GameData/achievements.txt";
+
+    public void Awake()
+    {
+        achievements = LoadAchievements();
+    }
+
+    public void SaveAchievementsAsync()
+    {
+        StreamWriter writer = new StreamWriter(path, true);
+        for (int i = 0; i < achievements.achievementList.Count; i++)
+        {
+            Achievement achievement = achievements.achievementList[i];
+            writer.WriteLine(achievement.title + ":" + achievement.description + ":" + achievement.isComplete.ToString());
+        }
+        writer.Close();
+    }
+
+    private Achievements LoadAchievements()
+    {
+        StreamReader reader = new(path);
+        Achievements achievements = new();
+        //Ignores Header line
+        reader.ReadLine();
+        while(!reader.EndOfStream)
+        {
+            string[] achievement = reader.ReadLine().Split(":");
+            bool isComplete = achievement[2] == true.ToString();
+            achievements.achievementList.Add(new(achievement[0], achievement[1], isComplete));
+        }
+        reader.Close();
+        return achievements;
+    }
+}
