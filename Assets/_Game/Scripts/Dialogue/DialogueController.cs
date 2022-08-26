dd6831dc0e458e5107c25f653f7680b811dfb9e3
Adrian Kovatana <adrian.kovatana@gmail.com>
2022-08-26 09:53:59 -0500

    Import and modify dialogue system

---
+++ Assets/_Game/Scripts/Dialogue/DialogueController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace DragonLens.BrackeysGameJam2022_2.Dialogue
+{
+	public class DialogueController : MonoBehaviour
+	{
+        public bool isDebugMode;
+        public TextMeshProUGUI dialogueText;
+        public TextMeshProUGUI dialogueActor;
+
+        public Animator animator;
+        private bool _isAnimating;
+
+        private Queue<DialogueMessage> _messages;
+        private DialogueMessage _currentMessage;
+
+        private void Awake()
+        {
+            _messages = new();
+        }
+
+        public void StartDialogue(DialogueData dialogue)
+        {
+            animator.SetBool("IsOpen", true);
+
+            _messages.Clear();
+
+            foreach(DialogueMessage message in dialogue.Messages)
+            {
+                _messages.Enqueue(message);
+            }
+
+            DisplayNextSentence();
+        }
+
+        public void DisplayNextSentence()
+        {
+            if(_isAnimating)
+            {
+                if(isDebugMode)
+                {
+                    StopAllCoroutines();
+                    _isAnimating = false;
+                    dialogueText.text = _currentMessage.Text;
+                    dialogueActor.text = _currentMessage.ActorName;
+                }
+                return;
+            }
+            else if(_messages.Count == 0)
+            {
+                EndDialogue();
+                return;
+            }
+
+            _currentMessage = _messages.Dequeue();
+            StopAllCoroutines();
+            StartCoroutine(AnimateSentence(_currentMessage));
+        }
+
+        IEnumerator AnimateSentence(DialogueMessage message)
+        {
+            _isAnimating = true;
+            dialogueText.text = "";
+            dialogueActor.text = message.ActorName;
+            yield return new WaitForSeconds(0.5f);
+
+            foreach(char letter in message.Text.ToCharArray())
+            {
+                dialogueText.text += letter;
+                yield return null;
+            }
+
+            _isAnimating = false;
+        }
+
+        void EndDialogue()
+        {
+            animator.SetBool("IsOpen", false);
+            // TODO: Notify listeners with a dialogue end event?
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Dialogue/DialogueData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DragonLens.BrackeysGameJam2022_2.Dialogue
+{
+    [CreateAssetMenu(menuName = "Game Jam/Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [SerializeField]
+        private DialogueMessage[] _messages;
+
+        public DialogueMessage[] Messages => _messages;
+    }
+
+    [System.Serializable]
+    public class DialogueMessage
+    {
+        [SerializeField]
+        private string _actorName;
+
+        [SerializeField, TextArea(5, 5)]
+        private string _text;
+
+        public string ActorName { get => _actorName; }
+        public string Text { get => _text; }
+    }
+}
